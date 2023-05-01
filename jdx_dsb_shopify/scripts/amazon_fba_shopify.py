c67e6bf27d42db52659666ca7a2a95f3456dd6be
allisonw <allison@junodx.com>
2023-05-01 14:30:24 -0700

    create only the top two for now

--- jdx_dsb_shopify/scripts/amazon_fba_shopify.py
+++ jdx_dsb_shopify/scripts/amazon_fba_shopify.py
@@ -13,6 +13,7 @@ from snowflake.snowpark import Session
 from jdx_dsb_shopify.globals import SNOWFLAKE_SECRET_NAME, SHOPIFY_SECRET_NAME, JOTFORM_SECRET_NAME, \
     JOTFORM_ID_HAZEL, JOTFORM_ID_BIRCH, INVENTORY_SHEET_ID, GOOGLE_API_SECRET_NAME, ORDER_CREATION_SHEET_ID, \
     SLACK_BOT_TOKEN, AMAZON_FBA_USER_SHEET_ID, FST_PRODUCT_ID
+from jdx_dsb_shopify.scripts.jotform_integration import get_b2b_orders, get_latest_product_variant_info
 from jdx_dsb_shopify.util.jotform_utils import JotformAPIClient, parse_form_names, parse_form_dates
 from jdx_dsb_shopify.util.logging import setup_logging_env
 from jdx_dsb_shopify.util.platform_db_utils import get_platformdb_conn_str
@@ -28,44 +29,6 @@ scopes = [
 ]
 google_creds = Credentials.from_service_account_info(creds, scopes=scopes)
 
-def get_amazon_fba_orders(
-        product_id,
-        first_name,
-        last_name,
-        email,
-        account_address
-
-):
-    if os.environ['ENV'] == 'dev':
-        test_flag = True
-    else:
-        test_flag = False
-
-    return {
-        "order": {
-            "line_items": [
-                {
-                    "product_id": product_id,
-                    "quantity": 1,
-                }
-            ],
-            "customer": {
-                "first_name": first_name,
-                "last_name": last_name,
-                "email": email
-            },
-            "email": email,
-            "phone": "858-201-7154",
-            "tags": "Sync: Failed,",
-            "billing_address": account_address,
-            "shipping_address": account_address,
-            "financial_status": "paid",
-            "send_receipt": False,
-            "send_fulfillment_receipt": False,
-            'test': test_flag,
-            "discount_codes": [{"code": "", "amount": "100.0", "type": "percentage"}]
-        }
-    }
 
 @log_start_stop
 @log_runtime
@@ -79,22 +42,55 @@ def amazon_fba_shopify():
         creds=google_creds,
     )
 
+    # get latest variant information
+    shopify_secrets = get_secret_from_sm(SHOPIFY_SECRET_NAME)
+    shop_env = shopify_secrets['SHOP_ENV']
+    variant_df = get_latest_product_variant_info(shop_env)
+
     # create order based on amazon FBA user creation sheet
     # Only create order if STATUS='REGISTERED'
-    new_orders = total_amazon_fba_orders.query('Status=="REGISTERED"')
+    new_orders = total_amazon_fba_orders.query('Status=="REGISTERED"').head(2)
+    new_orders['account_name'] = 'Amazon FBA'
+    new_orders['product_short_name'] = 'birch'
 
-    if len(new_orders)>0:
+    if len(new_orders) > 0:
         logger.info(f'Found {len(new_orders)} orders to create.')
-        shopify_order_names=list()
+        new_orders['account_name_sku'] = new_orders['account_name'] + '|' + new_orders['product_short_name']
+        variant_df['account_name_sku'] = variant_df['account_name'] + '|' + variant_df['product_short_name']
+        fuzzy_matched_df = fuzzy_merge(
+            new_orders, variant_df[['account_name_sku', 'id', 'product_id', 'price']],
+            'account_name_sku', 'account_name_sku',
+            threshold=90,
+            how='left'
+        ).rename(columns={'id': 'variant_id'})
+        fuzzy_matched_df_cols = [
+            'account_name',
+            'first_name',
+            'last_name',
+            'email',
+            'dob',
+            'lmp',
+            'product_short_name',
+            'product_id',
+            'variant_id',
+            'kit_code',
+            'order_submitted_at',
+        ]
+
+        shopify_order_names = list()
         shopify_order_ids = list()
-        for order in new_orders.iterrows():
+        shopify_order_date=list()
+        for order in fuzzy_matched_df.iterrows():
+            account_name = order[1]['account_name']
             first_name = order[1]['First Name']
-            last_name = order[1]['Lat Name']
+            last_name = order[1]['Last Name']
             email = order[1]['Email']
-            product_id = FST_PRODUCT_ID
+            variant_id = order[1]['variant_id']
+            product_id = order[1]['product_id']
             account_address = {
                 "first_name": first_name,
                 "last_name": last_name,
+                "company": account_name,
                 "address1": "11760 Sorrento Valley Rd Suite J",
                 "phone": "858-201-7154",
                 "city": "San Diego",
@@ -104,63 +100,41 @@ def amazon_fba_shopify():
             }
 
             order_payload = get_b2b_orders(
-                variant_id = variant_id,
-                product_id = product_id,
-                first_name = first_name,
-                last_name = last_name,
-                email = email,
-                account_address = account_address
+                variant_id=variant_id,
+                product_id=product_id,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                account_address=account_address
             )
             logger.info(f'Create order for {account_name} with email: {email}')
-            r=shopify_helper.create_order(order_payload)
-            if r.status_code in (200,201): #successfully created
+            r = shopify_helper.create_order(order_payload)
+            if r.status_code in (200, 201):  # successfully created
                 logger.info(f"Created shopify order: {r.json()['order']['name']}")
                 shopify_order_names.append(r.json()['order']['name'])
                 shopify_order_ids.append(r.json()['order']['id'])
+                shopify_order_date.append(r.json()['order']["created_at"])
             else:
                 shopify_order_names.append('')
                 shopify_order_ids.append('')
+                shopify_order_date.append('')
 
         shopify_order_created = pd.concat(
             [
                 fuzzy_matched_df[fuzzy_matched_df_cols],
                 pd.DataFrame(shopify_order_names, columns=['order_name']),
-                pd.DataFrame(shopify_order_ids, columns=['order_id'])
+                pd.DataFrame(shopify_order_ids, columns=['order_id']),
+                pd.DataFrame(shopify_order_ids, columns=['order_created_at']),
             ], axis=1
         )
 
-        # get inventory information
-        inventory_df = get_spreadsheet(INVENTORY_SHEET_ID, range='Providers', creds=google_creds)[
-            [
-                'Kit_Code',
-                'Device_ID',
-                'ReturnShipping',
-                'ExpDate'
-            ]
-        ]
 
-        inventory_df = inventory_df.rename(columns={
-            'Kit_Code': 'kit_code',
-            'Device_ID':'sample_number',
-            'ReturnShipping': 'return_tracking_number',
-            'ExpDate': 'expiration_date'
-        })
-
-        inventory_df['kit_code'] = inventory_df['kit_code'].apply(lambda x: x.upper())
-
-        shopify_order_created = shopify_order_created.merge(inventory_df, on='kit_code', how='left')
         update_cols = [
             'order_name',
-            'first_name',
-            'last_name',
-            'email',
-            'dob',
-            'lmp',
-            'kit_code',
-            'sample_number',
-            'return_tracking_number',
-            'expiration_date',
-            'order_submitted_at',
+            'First Name',
+            'Last Name',
+            'Email',
+            'order_created_at',
         ]
         response = append_df2gsheet(shopify_order_created[update_cols].fillna(''), google_creds, ORDER_CREATION_SHEET_ID)
         logger.info('Updated order creation report on Google drive:')
